f280b014801c6c5d081b46b660fb872a992d51da
Adrià Blancafort <adriablancafort@gmail.com>
2024-12-14 15:54:47 +0100

    try to call db from streamlit template

--- pages/profes.py
+++ pages/profes.py
@@ -1,2 +1,12 @@
 import streamlit as st
+from db import Database
+
 st.page_link("app.py", label="back", icon="1️⃣")
+
+db = Database()
+profes = db.get_collection("profes")
+
+# Extract and display all "name" values
+names = [profe['name'] for profe in profes]
+st.write("List of Profes Names:")
+st.write(names)
